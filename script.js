061a4382e270eb569ddf65a17bcb004871b45bbf
GhostLead <jkomcjkomc@gmail.com>
2024-09-05 09:51:16 +0200

    finito

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 "use strict"
+
+var textboxDiv = document.getElementById("sorok");
+    textboxDiv.innerHTML = "";
+    
+    
+
+    var valaszto = document.getElementById("honapvalaszto")
+    valaszto.addEventListener("change",(valami) =>{
+        for (let index = 0; index < parseInt(valami.target.value); index++) {
+            let divv = document.createElement("div");
+            divv.classList.add("cd");
+            let szam = document.createElement("p");
+            szam.innerHTML = index;
+            let textbox = document.createElement("input");
+            textbox.type = "text";
+            textbox.placeholder = "Írj ide valamit...";
+            divv.appendChild(szam);
+            textboxDiv.appendChild(divv);
+            textboxDiv.appendChild(textbox);
+        }
+    })
